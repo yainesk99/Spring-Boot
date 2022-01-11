15cdec725220056e4396fc4c742777eb28c3386b
Kseniya Chechulova <44057113+yainesk99@users.noreply.github.com>
2022-01-11 14:38:40 +0300

    Delete SpringSecurityInitializer.java

--- Spring-Boot/src/main/java/net/proselyte/springboot/config/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package net.proselyte.springboot.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
